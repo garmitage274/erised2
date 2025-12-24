c1f3084fda7f41da12f52d29224631a4d93f34d1
garmitage274 <58632068+garmitage274@users.noreply.github.com>
2025-12-24 18:37:44 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const images = [
+    "media1/kg2.PNG",
+    "media1/mollie.PNG",
+    "media1/shark.PNG",
+    "media1/kg.PNG",
+    "media1/pingu.PNG",
+    "media1/country.PNG",
+    "media1/dogs.PNG"
+];
+
+const home = document.getElementById("home");
+const slideshow = document.getElementById("slideshow");
+const slideImage = document.getElementById("slideImage");
+const startBtn = document.getElementById("startBtn");
+
+let currentIndex = 0;
+let slideshowInterval;
+
+startBtn.addEventListener("click", () => {
+    home.style.display = "none";
+    slideshow.style.display = "block";
+    currentIndex = 0;
+    showImage();
+    slideshowInterval = setInterval(nextImage, 10000);
+});
+
+function showImage() {
+    slideImage.src = images[currentIndex];
+}
+
+function nextImage() {
+    currentIndex++;
+
+    if (currentIndex >= images.length) {
+        clearInterval(slideshowInterval);
+        returnHome();
+    } else {
+        showImage();
+    }
+}
+
+function returnHome() {
+    slideshow.style.display = "none";
+    home.style.display = "flex";
+}
